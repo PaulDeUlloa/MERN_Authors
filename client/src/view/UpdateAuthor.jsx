79f1db395f050dacca1db56fa8aadd85d73b8782
Paul De Ulloa <pauld143@gmail.com>
2023-12-31 23:59:00 -0800

    editUpdateColorChange

--- client/src/view/UpdateAuthor.jsx
+++ client/src/view/UpdateAuthor.jsx
@@ -1,72 +1,68 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { useNavigate, useParams, } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-
-
 const UpdateAuthor = () => {
-    const[name, setName] = useState("");
+  const [name, setName] = useState("");
 
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const [errors, setErrors] = useState([])
+  const [errors, setErrors] = useState([]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        axios
-        .get(`http://localhost:8000/api/authors/${id}`)
-        .then((res) =>{
-            const oneAuthor = res.data;
-            setName(oneAuthor.name);
-        })
-        .catch((err) => console.log(err));
-        
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/authors/${id}`)
+      .then((res) => {
+        const oneAuthor = res.data;
+        setName(oneAuthor.name);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    const submitHandler = (e) => {
-        e.preventDefault();
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-        axios
-        .put(`http://localhost:8000/api/authors/${id}`, {name})
-        .then(() => navigate("/authors"))
-        .catch((err)=>{
-            const errResponse = err.response.data.errors;
-            const errArr = [];
-            for(const key in errResponse){
-                errArr.push(errResponse[key].message)
-            }
-            setErrors(errArr);
-        });
-    };
+    axios
+      .put(`http://localhost:8000/api/authors/${id}`, { name })
+      .then(() => navigate("/authors"))
+      .catch((err) => {
+        const errResponse = err.response.data.errors;
+        const errArr = [];
+        for (const key in errResponse) {
+          errArr.push(errResponse[key].message);
+        }
+        setErrors(errArr);
+      });
+  };
 
-
-
-    return <div>
-        <h2>Edit this author:</h2>
-        <form onSubmit={submitHandler}>
-            <div>
-                <label>Name: </label>
-                <br></br>
-                <input 
-                type="text" 
-                value={name} 
-                onChange={(e)=>setName(e.target.value)}
-                />
-                <div>
-                    <Link to="/authors">
-                        <button>Cancel</button>
-                    </Link>
-                    {" "}
-                    <input type="submit" value="Submit" />
-                </div>
-            </div>
-        </form>
-        {errors.map((err, idx) =>{
-            return <p key={idx}>{err}</p>;
-        })}
+  return (
+    <div>
+      <h2 id="editTitleColoring">Edit this author:</h2>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label>Name: </label>
+          <br></br>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <div>
+            <Link to="/authors">
+              <button>Cancel</button>
+            </Link>{" "}
+            <input type="submit" value="Submit" />
+          </div>
+        </div>
+      </form>
+      {errors.map((err, idx) => {
+        return <p key={idx}>{err}</p>;
+      })}
     </div>
+  );
 };
 
 export default UpdateAuthor;
