183311d31cb79aa8ab9c969d07b7117534ab5c5f
Paul De Ulloa <pauld143@gmail.com>
2023-12-31 23:25:26 -0800

    fav authors coloring change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,19 @@ import CreateAuthor from "./view/CreateAuthor";
 import UpdateAuthor from "./view/UpdateAuthor";
 
 function App() {
-  return <div className="App">
-    <h1>Favorite authors</h1>
-    <Link to="/authors">Home</Link> | {" "}
-    <Link to="/authors/new">Add an author</Link>
-    <hr />
-    <Routes>
-      <Route path="/authors" element={<Dashboard />} />
-      <Route path="/authors/new" element={<CreateAuthor />} />
-      <Route path="/authors/:id/edit" element={<UpdateAuthor />} />
-    </Routes>
-
-  </div>;
+  return (
+    <div className="App">
+      <h1 id="favAuthorsColoring">Favorite Authors</h1>
+      <Link to="/authors">Home</Link> |{" "}
+      <Link to="/authors/new">Add an author</Link>
+      <hr />
+      <Routes>
+        <Route path="/authors" element={<Dashboard />} />
+        <Route path="/authors/new" element={<CreateAuthor />} />
+        <Route path="/authors/:id/edit" element={<UpdateAuthor />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
