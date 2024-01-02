dbc16226c06134b82c35d2ab15666b63f328678a
Paul De Ulloa <pauld143@gmail.com>
2024-01-02 01:38:43 -0800

    edit and delete buttons adjustment

--- client/src/view/Dashboard.jsx
+++ client/src/view/Dashboard.jsx
@@ -46,10 +46,13 @@ const Dashboard = () => {
                   <td id="authorNameFont">{oneAuthor.name}</td>
                   <td id="editDeleteSpacing">
                     <Link to={`/authors/${oneAuthor._id}/edit`}>
-                      <button>Edit</button>
+                      <button class="btn btn-light">Edit</button>
                     </Link>
                     <Link>
-                      <button onClick={() => handleDelete(oneAuthor._id)}>
+                      <button
+                        class="btn btn-light"
+                        onClick={() => handleDelete(oneAuthor._id)}
+                      >
                         Delete
                       </button>
                     </Link>
