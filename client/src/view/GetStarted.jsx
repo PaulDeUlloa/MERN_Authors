4fb013a4a1f777dce700fb50f8695e5decd2ce16
Paul De Ulloa <pauld143@gmail.com>
2024-02-15 00:23:29 -0800

    image add, testing

--- client/src/view/GetStarted.jsx
+++ client/src/view/GetStarted.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 const GetStarted = () => {
   return (
-    <div id="getStartedPage">
-      <button class="btn btn-light">
-        <Link to="/authors" id="getStartedBtnStyling">
-          GET STARTED
-        </Link>
-      </button>
+    <div id="getStartedBackgroundImage">
+      <div id="getStartedPage">
+        <button class="btn btn-light">
+          <Link to="/authors" id="getStartedBtnStyling">
+            GET STARTED
+          </Link>
+        </button>
+      </div>
     </div>
   );
 };
