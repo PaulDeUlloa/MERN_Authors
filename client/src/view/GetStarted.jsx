763004f36048bca4f7f68515c33dd86f4fa92ad1
Paul De Ulloa <pauld143@gmail.com>
2024-02-18 23:27:26 -0800

    media queries and spacing for carousel

--- client/src/view/GetStarted.jsx
+++ client/src/view/GetStarted.jsx
@@ -11,97 +11,99 @@ const GetStarted = () => {
           </Link>
         </button>
       </div>
-      <div id="carouselExampleDark" class="carousel carousel-dark slide">
-        <div class="carousel-indicators">
+      <div id="carouselWrapper">
+        <div id="carouselExampleDark" class="carousel carousel-dark slide">
+          <div class="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark"
+              data-bs-slide-to="0"
+              class="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+              <img
+                src={require("./../images/jk_rowling.jpeg")}
+                class="d-block w-100"
+                alt="jk_rowling_picture"
+                width="100"
+                height="400"
+              />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>J.K. Rowling</h5>
+                <p>
+                  Ms. Rowling has a style of writing that paved a way into the
+                  annals of literary history.
+                </p>
+              </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+              <img
+                src={require("./../images/stephen_king.jpeg")}
+                class="d-block w-100"
+                alt="stephen_king_picture"
+                width="100"
+                height="400"
+              />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Stephen King</h5>
+                <p>
+                  Stephen E. King is an American author of horror, supernatural
+                  fiction, suspense, crime, science-fiction, and fantasy novels.
+                </p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img
+                src={require("./../images/ken_follett.jpeg")}
+                class="d-block w-100"
+                alt="ken_follet_picture"
+                width="100"
+                height="400"
+              />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Ken Follet</h5>
+                <p>
+                  Kenneth M. Follett is a Welsh author of thrillers and
+                  historical novels who has sold more than 160 million copies.
+                </p>
+              </div>
+            </div>
+          </div>
           <button
+            class="carousel-control-prev"
             type="button"
             data-bs-target="#carouselExampleDark"
-            data-bs-slide-to="0"
-            class="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
           <button
+            class="carousel-control-next"
             type="button"
             data-bs-target="#carouselExampleDark"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="10000">
-            <img
-              src={require("./../images/jk_rowling.jpeg")}
-              class="d-block w-100"
-              alt="jk_rowling_picture"
-              width="100"
-              height="400"
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>J.K. Rowling</h5>
-              <p>
-                Ms. Rowling has a style of writing that paved a way into the
-                annals of literary history.
-              </p>
-            </div>
-          </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img
-              src={require("./../images/stephen_king.jpeg")}
-              class="d-block w-100"
-              alt="stephen_king_picture"
-              width="100"
-              height="400"
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Stephen King</h5>
-              <p>
-                Stephen E. King is an American author of horror, supernatural
-                fiction, suspense, crime, science-fiction, and fantasy novels.
-              </p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img
-              src={require("./../images/ken_follett.jpeg")}
-              class="d-block w-100"
-              alt="ken_follet_picture"
-              width="100"
-              height="400"
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Ken Follet</h5>
-              <p>
-                Kenneth M. Follett is a Welsh author of thrillers and historical
-                novels who has sold more than 160 million copies.
-              </p>
-            </div>
-          </div>
-        </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
     </div>
   );
