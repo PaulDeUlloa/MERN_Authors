d932c5c37ed21389892eec11265c884c33b7cf0c
Paul De Ulloa <pauld143@gmail.com>
2024-02-26 23:54:07 -0800

    createAuthor page has description added pending rendering for edit page

--- client/src/view/CreateAuthor.jsx
+++ client/src/view/CreateAuthor.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 const CreateAuthor = () => {
   const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
 
   const [errors, setErrors] = useState([]);
 
@@ -14,7 +15,7 @@ const CreateAuthor = () => {
     e.preventDefault();
 
     axios
-      .post(`http://localhost:8000/api/authors`, { name })
+      .post(`http://localhost:8000/api/authors`, { name, description })
       .then(() => navigate("/authors"))
       .catch((err) => {
         const errResponse = err.response.data.errors;
@@ -37,6 +38,16 @@ const CreateAuthor = () => {
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div id="editAndCreateLabelSpacing">
+            <label id="updateAndEditLabels">Description: </label>
+            <textarea
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              rows="3"
+              cols="25"
             />
           </div>
           <div id="buttonSpacing">
