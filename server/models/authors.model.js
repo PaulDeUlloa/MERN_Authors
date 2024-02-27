e4f3bab0b288578d61e4a41e30ecfaf642e95f0a
Paul De Ulloa <pauld143@gmail.com>
2024-02-26 23:43:12 -0800

    testing description for authors

--- server/models/authors.model.js
+++ server/models/authors.model.js
@@ -7,6 +7,11 @@ const AuthorSchema = new mongoose.Schema(
       required: [true, "Name is required."],
       minLength: [3, "Authors name must be 3+ characters"],
     },
+    description: {
+      type: String,
+      required: [true, "Description is required."],
+      minLength: [5, "Authors name must be 5+ characters"],
+    },
   },
   { timestamps: true }
 );
