c62f2bc8241a7f2e108ffade586cf9318b40c903
Paul De Ulloa <pauld143@gmail.com>
2024-02-27 23:43:21 -0800

    successfully added a route for a details page of each individual author, pending button to view a specific author

--- client/src/view/Detail.jsx
+++ client/src/view/Detail.jsx
@@ -1,29 +1,27 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Detail = () => {
-  const [authorList, setAuthorList] = useState([]);
+const Detail = (props) => {
+  const [oneAuthor, setOneAuthor] = useState({});
 
   const { id } = useParams();
 
   useEffect(() => {
     axios
-      .get(`http://localhost:8000/api/authors/${id}`)
-      .then((res) => setAuthorList(res.data))
+      .get("http://localhost:8000/api/authors/" + id)
+      .then((res) => setOneAuthor(res.data))
       .catch((err) => console.log(err));
   }, []);
 
-  {
-    authorList.map((oneAuthor, idx) => {
-      return (
-        <div id="detailsPageCentering">
-          <p>Name: {oneAuthor.name}</p>
-          <p>Description: {oneAuthor.description}</p>
-        </div>
-      );
-    });
-  }
+  return (
+    <div id="detailsPageCentering">
+      <p>Name: {oneAuthor.name}</p>
+      <p>Description: {oneAuthor.description}</p>
+      <Link to={"/authors"}>Home</Link>
+    </div>
+  );
 };
 
 export default Detail;
