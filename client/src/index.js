230432088ea8958d28b1cfec442ce0cdd3379816
Paul De Ulloa <pauld143@gmail.com>
2024-03-10 22:58:32 -0700

    index.js file updated with AuthProvider and notes

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
+
+//Authprovider was wrapped around the browser router and app so that every component will have access to the state and the dispatch function
