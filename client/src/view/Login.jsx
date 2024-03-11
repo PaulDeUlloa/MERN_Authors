22d2ba3abdb9b255b4e6d0a246ba26dcc4efb2f4
Paul De Ulloa <pauld143@gmail.com>
2024-03-10 23:55:48 -0700

    Login.jsx file created, draft

---
+++ client/src/view/Login.jsx
@@ -0,0 +1,38 @@
+// registerUser(registerForm)
+//       .then((userData) => {
+//         console.log(userData);
+//         dispatch({ type: "LOGIN", payload: userData });
+//         localStorage.setItem("user", JSON.stringify(userData));
+//       })
+//       .catch((err) => console.log(err));
+
+function Login() {
+  return (
+    <div id="loginWrapper">
+      <section>
+        <h1>Login</h1>
+        <div>
+          <form>
+            <div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input name="email" id="email" type="text" />
+              </div>
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input name="password" id="password" type="text" />
+              </div>
+              <button
+                class="btn btn-light"
+                type="submit"
+                id="createAndEditLinksFontColor"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+}
