0de18fa263bf1d50c7d00df82634ef311f076890
Paul De Ulloa <pauld143@gmail.com>
2024-03-10 23:42:57 -0700

    bug fix and notes added

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 export const AuthContext = createContext({
   state: null,
@@ -21,6 +21,16 @@ function authReducer(state, action) {
 //useReducer(authReducer, { user: null }); <-- that is the initial state.
 function AuthProvider({ children }) {
   const [state, dispatch] = useReducer(authReducer, { user: null });
+
+  // JSON.parse = Converts a JavaScript Object Notation (JSON) string into an object.
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, [state.user?.id]);
+  // The ? i s an optional operator which is saying if there is an Id that's okay and if there's NOT an Id thats okay too.
+
   return (
     <AuthContext.Provider value={{ state, dispatch }}>
       {children}
